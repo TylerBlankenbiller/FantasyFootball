33c1c2ba52e6da3fbcf4b6c14697cababad3f716
TylerBlankenbiller <33728690+TylerBlankenbiller@users.noreply.github.com>
2019-01-22 16:14:31 -0500

    fixed a few things, almost done

--- createCSV.py
+++ createCSV.py
@@ -18,35 +18,42 @@ throw = data
 throw = throw.loc[throw['penalty'] == 0]
 throw = throw.loc[throw['play_type'] == 'pass']
 throw['tattempts'] = 1
+throw['player'] = throw['passer_player_name']
 throw['completions'] = throw['tattempts'] - throw['incomplete_pass']
-throw = throw.groupby(['passer_player_name', 'posteam']).sum()
+#throw = throw.groupby(['passer_player_id', 'posteam']).sum()
 throw['percent'] = throw['completions']/throw['tattempts']
 throw['y/att'] = throw['yards_gained']/throw['tattempts']
-throw = throw.sort_values(['yards_gained'])
+#throw = throw.sort_values(['yards_gained'])
 throw['passYards'] = throw['yards_gained']
-throw['player'] = throw['passer_player_name']
 #print(throw)
 #print(throw['sack'])
 
 run = data
 run = run.loc[run['penalty'] == 0]
 run = run.loc[run['play_type'] == 'run']
+run['name'] = run['rusher_player_name']
 run['rattempts'] = 1
-run = run.groupby(['rusher_player_name', 'posteam']).sum()
-run = run.sort_values(['yards_gained'])
+#run = run.groupby(['rusher_player_name', 'posteam']).sum()
+#run = run.sort_values(['yards_gained'])
 run['runYards'] = run['yards_gained']
 run['rAVG'] = run['runYards']/run['rattempts']
 run['rTD'] = run['touchdown']
-#run['name'] = run['rusher_player_name']
 #print(run['rattempts'], run['runYards'], run['rAVG'], run['rTD'], run['fumble'], run['fumble_lost'])
 
 receiver = data
 receiver = receiver.loc[receiver['penalty'] == 0]
 receiver = receiver.loc[receiver['play_type'] == 'pass']
+receiver['name'] = receiver['receiver_player_name'] 
 receiver['target'] = 1
-receiver = receiver.groupby(['receiver_player_name', 'posteam']).sum()
-receiver = receiver.sort_values(['yards_gained'])
-#receiver['name'] = receiver['receiver_player_name']
+#receiver = receiver.groupby(['receiver_player_name', 'posteam']).sum()
+#receiver = receiver.sort_values(['yards_gained'])
 print(receiver)
 
-#players = pd.concat([throw, run, receiver], axis=0, ignore_index=True)
+
+
+
+players = pd.concat([throw, run, receiver], axis=0, ignore_index=True, sort = False)
+players = players.groupby(['name', 'posteam']).sum()
+players = players.sort_values(['yards_gained'])
+#players = receiver.groupby(['name', 'posteam']).sum()
+print(players)
