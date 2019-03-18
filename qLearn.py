cfe714fc1c813835fe836b7b9adbb87185b9845b
TylerBlankenbiller <33728690+TylerBlankenbiller@users.noreply.github.com>
2019-03-18 11:47:31 -0400

    Add files via upload

--- qLearn.py
+++ qLearn.py
@@ -10,129 +10,38 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+tf.enable_eager_execution()
+
 print(tf.__version__)
 
+training_df: pd.DataFrame = pd.read_csv("final.csv", low_memory=False)
 
-raw_dataset = pd.read_csv("final.csv", low_memory=False)
-columnsUse = ['home_team', 'away_team', 'posteam', 'defteam', 'play_type',
-            'pass_location', 'run_gap', 'field_goal_result', 'extra_point_result', 'two_point_conv_result',
-            'timeout_team', 'td_team', 'passer_player_name', 'receiver_player_name', 'rusher_player_name', 
-            'kicker_player_name',
-            'penalty_type',
-            'Weather', 'WDirection', 'HCoach', 'HDefense', 'HOffense', 'ACoach', 'ADefense', 'AOffense']
+training_df['home_team'] = 'H' + training_df['home_team'].astype(str)
+training_df['away_team'] = 'A' + training_df['away_team'].astype(str)
+training_df['posteam'] = 'P' + training_df['posteam'].astype(str)
+training_df['defteam'] = 'D' + training_df['defteam'].astype(str)
+training_df['pass_location'] = 'Pass' + training_df['pass_location'].astype(str)
+training_df['run_location'] = 'Run' + training_df['run_location'].astype(str)
+training_df['extra_point_result'] = 'Extra' + training_df['extra_point_result'].astype(str)
+training_df['two_point_conv_result'] = 'Two' + training_df['two_point_conv_result'].astype(str)
+training_df[''] = '' + training_df[''].astype(str)
+training_df[''] = '' + training_df[''].astype(str)
+training_df[''] = '' + training_df[''].astype(str)
+training_df[''] = '' + training_df[''].astype(str)
+training_df[''] = '' + training_df[''].astype(str)
+print(type(training_df))
 
-raw_dataset = pd.get_dummies(raw_dataset, columns = columnsUse)
-                      
-dataset = raw_dataset.copy()
-dataset.tail()
-print(type(dataset))
+#features = ['
 
-#dataset = dataset.sample(frac=0.01,random_state=0)
-
-train_dataset = dataset.sample(frac=0.8,random_state=0)
-print(type(train_dataset))
-test_dataset = dataset.drop(train_dataset.index)
-
-train_stats = train_dataset.describe()
-train_stats.pop("no_huddle")
-train_stats = train_stats.transpose()
-print(train_stats)
-
-train_labels = train_dataset.pop('no_huddle')
-test_labels = test_dataset.pop('no_huddle')
+#target = ['
 
 
-def norm(x):
-    return (x - train_stats['mean']) / train_stats['std']
-normed_train_data = norm(train_dataset)
-normed_test_data = norm(test_dataset)
-
-def build_model():
-    model = keras.Sequential([
-        layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
-        layers.Dense(64, activation=tf.nn.relu),
-        layers.Dense(1)
-    ])
-
-    optimizer = tf.keras.optimizers.RMSprop(0.0001)
-
-    model.compile(loss='mean_squared_error',
-                optimizer=optimizer,
-                metrics=['mean_absolute_error', 'mean_squared_error'])
-    return model
-  
-model = build_model()
-
-model.summary()
-
-example_batch = normed_train_data[:10]
-example_result = model.predict(example_batch)
-example_result
 
 
-# Display training progress by printing a single dot for each completed epoch
-class PrintDot(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs):
-        if epoch % 100 == 0: print('')
-        print('.', end='')
-
-EPOCHS = 1000
-
-history = model.fit(
-      normed_train_data, train_labels,
-      epochs=EPOCHS, validation_split = 0.2, verbose=0,
-      callbacks=[PrintDot()])
-      
-hist = pd.DataFrame(history.history)
-hist['epoch'] = history.epoch
-hist.tail()
 
 
-def plot_history(history):
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
-    
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [no_huddle]')
-    plt.plot(hist['epoch'], hist['mean_absolute_error'],
-             label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
-             label = 'Val Error')
-    plt.ylim([0,5])
-    plt.legend()
-    
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$no_huddle^2$]')
-    plt.plot(hist['epoch'], hist['mean_squared_error'],
-             label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_squared_error'],
-             label = 'Val Error')
-    plt.ylim([0,20])
-    plt.legend()
-    plt.show()
 
 
-plot_history(history)
 
-odel = build_model()
 
-# The patience parameter is the amount of epochs to check for improvement
-early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-history = model.fit(normed_train_data, train_labels, epochs=EPOCHS,
-                    validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
-
-plot_history(history)
-
-test_predictions = model.predict(normed_test_data).flatten()
-
-plt.scatter(test_labels, test_predictions)
-plt.xlabel('True Values [MPG]')
-plt.ylabel('Predictions [MPG]')
-plt.axis('equal')
-plt.axis('square')
-plt.xlim([0,plt.xlim()[1]])
-plt.ylim([0,plt.ylim()[1]])
-_ = plt.plot([-100, 100], [-100, 100])
