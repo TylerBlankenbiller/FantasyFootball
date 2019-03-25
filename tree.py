04e68edfa0f048699ef7926e2cb304323aca31d2
TylerBlankenbiller <33728690+TylerBlankenbiller@users.noreply.github.com>
2019-03-25 11:46:06 -0400

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import sklearn.model_selection
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
+balance_data = pd.read_csv("given.csv", low_memory=False)
+
+print("Dataset Lenght:: ", len(balance_data))
+print("Dataset Shape:: ", balance_data.shape)
+
+
+print("Dataset:: ", balance_data.head())
+
+#Predict
+X = balance_data.values[:, 0:41]
+#Outcome
+Y = balance_data.values[42:102]
+
+X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+
+clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+                               max_depth=3, min_samples_leaf=5)
+clf_gini.fit(X_train, y_train)
+
+y_pred = clf_gini.predict(X_test)
+
+print("Accuracy is ", accuracy_score(y_test,y_pred)*100)
