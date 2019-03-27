1a6e6a3d429fa69fdb1933b6de22c95a874de9cc
TylerBlankenbiller <33728690+TylerBlankenbiller@users.noreply.github.com>
2019-03-27 10:56:31 -0400

    Add files via upload

--- tree.py
+++ tree.py
@@ -19,20 +19,75 @@ print("Dataset Shape:: ", balance_data.shape)
 print("Dataset:: ", balance_data.head())
 
 #Predict
-X = balance_data.values[:, 0:41]
-temp = onehot_encoder.fit_transform(X[:, 0:5].astype(str))#+X[:, 6:9]+onehot_encoder.fit_transform(X[:, 10].astype(str))+X[:, 11:29]+onehot_encoder.fit_transform(X[:, 30:32].astype(str))+X[:, 33]+onehot_encoder.fit_transform(X[:, 34].astype(str))+X[:, 35]+onehot_encoder.fit_transform(X[:, 36:41].astype(str))
+X = balance_data.values[:, 0:42]
+temp = onehot_encoder.fit_transform(X[:, 0:6].astype(str))#+X[:, 6:9]+onehot_encoder.fit_transform(X[:, 10].astype(str))+X[:, 11:29]+onehot_encoder.fit_transform(X[:, 30:32].astype(str))+X[:, 33]+onehot_encoder.fit_transform(X[:, 34].astype(str))+X[:, 35]+onehot_encoder.fit_transform(X[:, 36:41].astype(str))
 Predict = temp
-temp = X[:, 6:9]
-Predict= np.concatenate((Predict, temp))
-
-print(X)
+print(Predict)
+temp = X[:, 6:10]
+Predict= np.append(Predict, temp, 1)
+print(Predict)
+temp = onehot_encoder.fit_transform(X[:, 10:11].astype(str))
+#temp = temp.reshape(-1, 1)
+print(Predict)
+Predict= np.append(Predict, temp, 1)
+temp = X[:, 11:30]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(X[:, 30:33].astype(str))
+Predict= np.append(Predict, temp, 1)
+temp = X[:, 33:34]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(X[:, 34:35].astype(str))
+Predict= np.append(Predict, temp, 1)
+temp = X[:, 35:36]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(X[:, 36:42].astype(str))
+Predict= np.append(Predict, temp, 1)
+X = Predict
+print(Predict)
 
 #Outcome
-Y = balance_data.values[:, 42:129]
-Y = onehot_encoder.fit_transform(Y.astype(str))
+temp = balance_data.values[:, 126:129]
+Predict = temp
+temp = onehot_encoder.fit_transform(balance_data.values[:, 124:126].astype(str))
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 123:124]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 121:123].astype(str))
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 120:121]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 117:120].astype(str))#run location is first
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 115:117]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 114:115].astype(str))
+Predict= np.append(Predict, temp, 1)
+print("HERE1")
+temp = balance_data.values[:, 107:117]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 106:107].astype(str))#play type
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 103:106]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 101:103].astype(str))#replay
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 99:101]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 98:99].astype(str))#penalty team
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 97:98]
+Predict= np.append(Predict, temp, 1)
+temp = onehot_encoder.fit_transform(balance_data.values[:, 93:97].astype(str))#player ID
+Predict= np.append(Predict, temp, 1)
+temp = balance_data.values[:, 42:93]
+Predict= np.append(Predict, temp, 1)
+Y = Predict
+del Predict
+del temp
+del balance_data
+
 print("HERE")
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100)
-
 
 clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
                                max_depth=3, min_samples_leaf=5)
